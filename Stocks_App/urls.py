6b26a6ce4597580329235fae1ce1cc54930683c3
GilCaplan <rockycaplan@gmail.com>
2024-03-10 11:45:07 +0200

    started django project

--- Stocks_App/urls.py
+++ Stocks_App/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('Add_Transaction/', views.Add_Transaction, name="Add_Transaction"),
     path('Buy_Stocks/', views.Buy_Stocks, name="Buy_Stocks"),
     path('Query_results/', views.Query_results, name="Query_results"),
+    path('errorPage/', views.errorPage, name="errorPage")
     ]
--- Stocks_App/views.py
+++ Stocks_App/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Transactions
 # Create your views here.
 
 
@@ -7,8 +7,24 @@ def index(request):
     return render(request, 'index.html')
 
 
+def errorPage(request):
+    return render(request, 'errorPage.html')
+
+
 def Add_Transaction(request):
-    return render(request, 'Add_Transaction.html')
+    input_ID = request.POST.get('id')
+
+    if not Transactions.objects.filter(ID=input_ID).exists():
+        return render(request, 'errorPage.html')  # Create a template for id_exists_error.html
+
+    # If the ID doesn't exist, create a new transaction
+    transaction = Transactions()
+    transaction.id = input_ID
+    transaction.tamount = request.POST.get('transactionSum')
+    transaction.save()
+
+    recent_transactions = Transactions.objects.order_by('-tDate')[:10]
+    return render(request, 'Add_Transaction.html', {'recent_transactions': recent_transactions})
 
 
 def Buy_Stocks(request):
