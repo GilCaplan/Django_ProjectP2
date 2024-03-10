80183e4c407c8880a7d60b8f266b0c415ab9a014
GilCaplan <rockycaplan@gmail.com>
2024-03-10 11:12:24 +0200

    started django project

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -76,10 +76,6 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'mssql',
         'NAME': 'gil0caplan',
