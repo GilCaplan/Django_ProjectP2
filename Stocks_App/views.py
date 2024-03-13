7f38852880112c0ad8ea92905dd2f535e35991aa
raque <raquelgold02@gmail.com>
2024-03-13 12:12:52 +0200

    13/3

--- Stocks_App/views.py
+++ Stocks_App/views.py
@@ -3,6 +3,8 @@ from .models import Transactions, Stock, Buying, Company, Investor
 from django.db import connection
 from django import forms
 from django.db.models import Max
+import datetime
+
 
 # Create your views here.
 
@@ -25,7 +27,6 @@ def Add_Transaction(request):
         # Handle the case where the input is not a valid integer or is None
         flag = False
 
-
     if input_ID is not None and not Transactions.objects.filter(id=input_ID).exists() or flag:
         return render(request, 'errorPage.html')  # Create a template for id_exists_error.html
     if input_ID is not None:
@@ -45,8 +46,20 @@ def Buy_Stocks(request):
     BQuantity = request.POST.get('quantity')
     action = Buying.objects.order_by('-tdate')[:10]
 
-    if ID is None or Company is None or BQuantity is None or not Investor.objects.filter(id=ID).exists() or not Company.objects.filter(symbol=symbol).exists() or int(BQuantity) <= 0:
+    try:
+        investor = Investor.objects.get(id=ID)
+        company = Company.objects.get(symbol=symbol)
+    except Investor.DoesNotExist:
+        error_message = "Investor does not exist"
+        return render(request, 'buy_stocks.html', {'error_message': error_message, 'recent_transactions': action})
+    except Company.DoesNotExist:
+        error_message = "Company does not exist"
+        return render(request, 'buy_stocks.html', {'error_message': error_message, 'recent_transactions': action})
+
+    if BQuantity is None or int(BQuantity) <= 0:
         return render(request, 'buy_stocks.html', {'recent_transactions': action})
+
+    BQuantity = int(BQuantity)
     company = Company.objects.get(pk=symbol)
     date = Stock.objects.order_by('-tdate').first().tdate
     try:
@@ -57,8 +70,14 @@ def Buy_Stocks(request):
     if Buying.objects.filter(id=ID, symbol=symbol, tdate=date).exists():
         error_message = "Investor cannot make multiple purchases for the same company on the same day"
         return render(request, 'buy_stocks.html', {'error_message': error_message, 'recent_transactions': action})
-    investor = Investor._default_manager.get(id=ID)
-    investor.amount = price * int(BQuantity)
+
+
+    cost = price * BQuantity
+    if investor.amount < cost:
+        error_message = "Investor does not have enough money to make this purchase."
+        return render(request, 'buy_stocks.html', {'error_message': error_message, 'recent_transactions': action})
+
+    investor.amount -= cost
     investor.save()
 
     purchase = Buying()
@@ -70,16 +89,17 @@ def Buy_Stocks(request):
 
     purchase.symbol = Stock(symbol=Company.objects.get(pk=symbol))
     purchase.save()
+
     return render(request, 'buy_stocks.html', {'recent_transactions': action})
 
 def Query_results(request):
-    with connection.cursor () as cursor:
+    with connection.cursor() as cursor:
         with open('view_queries.sql', 'r') as sql_file:
             view_creation_commands = sql_file.read()
 
         cursor.execute(view_creation_commands)
 
-        #Query a
+        # Query a
         cursor.execute("""
             SELECT VarInv.Name, ROUND(SUM(Buying.BQuantity * Stock.Price), 3) AS spent
             FROM VarInv, Buying, Stock, Investor
@@ -90,7 +110,7 @@ def Query_results(request):
         """)
         query_result_a = dictfetchall(cursor)
 
-        #Query b
+        # Query b
         cursor.execute("""
         SELECT popQuantity.Symbol AS Symbol, Investor.Name as Name, popQuantity.quantity as Quantity
         FROM popQuantity, Investor
@@ -100,7 +120,7 @@ def Query_results(request):
 
         query_result_b = dictfetchall(cursor)
 
-        #Query c
+        # Query c
         cursor.execute("""
             SELECT Company.Symbol AS Symbol, COUNT(DISTINCT Buying.ID) as BuyersNumber
             FROM Company
@@ -125,4 +145,3 @@ def dictfetchall(cursor):
     "Return all rows from a cursor as a dict"
     columns = [col[0] for col in cursor.description]
     return [dict(zip(columns, row)) for row in cursor.fetchall()]
-
